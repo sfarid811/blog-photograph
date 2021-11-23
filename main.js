7c32d8fe5add7b2e109cba77f9e39ce2a14c80fa
Saad <saadfarid029@gmail.com>
2021-11-23 17:27:00 +0100

    Glassmorphism header scroll

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// const header = document.querySelector('header');
+const header = document.querySelector('header');
 
 
-// window.addEventListener('scroll', () => {
-//      const scroll = window.scrollY;
+window.addEventListener('scroll', () => {
+     const scroll = window.scrollY;
 
-//         if(scroll > 70) {
-//             header.classList.add('active');
-//         }
-//         else  {
-//             header.classList.remove('active');
+        if(scroll > 70) {
+            header.classList.add('active');
+        }
+        else  {
+            header.classList.remove('active');
             
-//         }
-// });
+        }
+});
 
